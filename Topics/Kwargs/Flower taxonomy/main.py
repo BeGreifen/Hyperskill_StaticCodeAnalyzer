3bfe09571bd762cf9af72cd4f804339a8f9330ff
gnubrew <b.greifen@gmail.com>
2023-07-25 10:14:33 +0200

    Topics solutions - add Iris, a nicer version

--- Topics/Kwargs/Flower taxonomy/main.py
+++ Topics/Kwargs/Flower taxonomy/main.py
@@ -1,16 +1,11 @@
 iris = {}
 
 
-def add_iris(*args, **kargs):
-    global iris
-    id_n = args[0]
-    flower_details = {}
-    flower_details["species"] = args[1]
-    flower_details["petal_length"] = args[2]
-    flower_details["petal_width"] = args[3]
-    flower_details.update(**kargs)
-    iris[id_n] = flower_details
-    pass
+def add_iris(id_n: int, species: str, petal_length: float, petal_width: float, **kwargs):
+    iris[id_n] = {"species": species,
+                  "petal_length": petal_length,
+                  "petal_width": petal_width,
+                  **kwargs}
 
-# x = add_iris(0, 'Iris versicolor', 4.0, 1.3, petal_hue='pale lilac')
-#   print(iris)
+#  _ = add_iris(0, 'Iris versicolor', 4.0, 1.3, petal_hue='pale lilac')
+#  print(iris)
